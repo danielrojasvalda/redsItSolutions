5b10f2cce56d9f5f9eb0014948958e2a5f2a308a
Daniel Rojas <danielrojasvalda@gmail.com>
2020-05-12 10:01:50 -0400

    Se agrega nueva versión con tienda online y equipo

--- dr/js/contact_me.js
+++ dr/js/contact_me.js
@@ -20,7 +20,8 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "../mail/contact_me.php",
+        
         type: "POST",
         data: {
           name: name,
@@ -35,7 +36,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Su mensaje ha sido enviado. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
