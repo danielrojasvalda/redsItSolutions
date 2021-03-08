90455e30446100780f242e9d729659ec02662b21
Freddy Daniel Rojas <danielrojasvalda@gmail.com>
2021-03-08 16:00:25 -0500

    Project Reds Solutions v english

---
+++ js/floating-wpp.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">  
+   $(function () {
+           $('#WAButton').floatingWhatsApp({
+               phone: '+59178893107', //WhatsApp Business phone number
+               headerTitle: 'Escríbenos por WhatsApp!', //Popup Title
+               popupMessage: 'Hola, en qué podemos ayudarte?', //Popup Message
+               showPopup: true, //Enables popup display
+               buttonImage: '<img src="../uploads/whatsapp.svg" />', //Button Image
+               //headerColor: 'crimson', //Custom header color
+               //backgroundColor: 'crimson', //Custom background button color
+               position: "left" //Position: left | right
+           })
+       })
+</script>  
